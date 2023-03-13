0ec6f1063111c77953dc9061d13273249b3fcb33
MahmoudNehro <mahmoudnehro9000@gmail.com>
2023-03-13 12:10:25 +0200

    Fix

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\OtpSent;
+use App\Listeners\OtpSentListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -29,6 +31,9 @@ class EventServiceProvider extends ServiceProvider
         ResetPasswordCreated::class => [
             SendResetPasswordCode::class,
         ],
+        OtpSent::class => [
+            OtpSentListener::class
+        ]
     ];
 
     /**
