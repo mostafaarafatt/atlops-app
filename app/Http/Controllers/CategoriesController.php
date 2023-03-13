53a24e7202ff236955272528bbdd54e887bc0aea
Mostafa arafat <mostafaarafat199@gmail.com>
2023-03-13 12:59:55 +0200

    fix

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -16,6 +16,7 @@ class CategoriesController extends Controller
     public function index()
     {
 
+        
         $categories = Category::all();
         return view('frontend.categories.categories', compact('categories'));
     }
